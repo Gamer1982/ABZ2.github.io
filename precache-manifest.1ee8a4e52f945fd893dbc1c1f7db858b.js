f95036f3ec35267699ee83e5d38d6ed753e59a31
Gamer1982 <pplazy1982@gmail.com>
2020-11-25 16:00:57 +0300

    first commit

---
+++ precache-manifest.1ee8a4e52f945fd893dbc1c1f7db858b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4de488b2afd3fac326d",
+    "url": "/ABZ2.github.io/css/app.2c34564a.css"
+  },
+  {
+    "revision": "4b3a4798c26d074b38bebf44db9505bb",
+    "url": "/ABZ2.github.io/fonts/OpenSans-Regular.4b3a4798.woff2"
+  },
+  {
+    "revision": "e56192fe213565ce3d8a7d4f353ecb30",
+    "url": "/ABZ2.github.io/fonts/OpenSans-Regular.e56192fe.woff"
+  },
+  {
+    "revision": "7073c63a31bf25e1c32c3cef23ef1ef4",
+    "url": "/ABZ2.github.io/fonts/OpenSans-SemiBold.7073c63a.woff"
+  },
+  {
+    "revision": "80f232ab6a9dc3a92366f8c1ca4961c3",
+    "url": "/ABZ2.github.io/fonts/OpenSans-SemiBold.80f232ab.woff2"
+  },
+  {
+    "revision": "97e6992da6693667061cc95dbf11bf50",
+    "url": "/ABZ2.github.io/fonts/Overpass-SemiBold.97e6992d.woff2"
+  },
+  {
+    "revision": "e938402b1cc4ca392d6932e0e2b8ac88",
+    "url": "/ABZ2.github.io/fonts/Overpass-SemiBold.e938402b.woff"
+  },
+  {
+    "revision": "010b6f1bd8f738baa3426a392e63cdd1",
+    "url": "/ABZ2.github.io/fonts/PTSans-Bold.010b6f1b.woff"
+  },
+  {
+    "revision": "86ad7741f3b8a01d647844860c63e74d",
+    "url": "/ABZ2.github.io/fonts/PTSans-Bold.86ad7741.woff2"
+  },
+  {
+    "revision": "2c08a565752b84c368ede8c8952e96ea",
+    "url": "/ABZ2.github.io/fonts/PTSans-Regular.2c08a565.woff"
+  },
+  {
+    "revision": "9b5ae415d6f802c6d907772282a38702",
+    "url": "/ABZ2.github.io/fonts/PTSans-Regular.9b5ae415.woff2"
+  },
+  {
+    "revision": "4d4e2a67ecc979106001063f7bc0a54e",
+    "url": "/ABZ2.github.io/fonts/SourceSansPro-Regular.4d4e2a67.woff"
+  },
+  {
+    "revision": "ac69c558290945e73c97aea26ea48fc5",
+    "url": "/ABZ2.github.io/fonts/SourceSansPro-Regular.ac69c558.woff2"
+  },
+  {
+    "revision": "0c9f4488cf03fc6628b23ca75e5e19a9",
+    "url": "/ABZ2.github.io/img/banner-photo.0c9f4488.webp"
+  },
+  {
+    "revision": "71b6ce8c974d4a0abb45c9a1a9f7c405",
+    "url": "/ABZ2.github.io/img/banner-photo.71b6ce8c.jpg"
+  },
+  {
+    "revision": "8204a3a4d9570ccdcbd1b101f714f211",
+    "url": "/ABZ2.github.io/img/banner-photo_1024.8204a3a4.webp"
+  },
+  {
+    "revision": "df0963ce3d172ac39f52dde546ae2339",
+    "url": "/ABZ2.github.io/img/banner-photo_1366.df0963ce.webp"
+  },
+  {
+    "revision": "9f44e8d6a6a8073004820b1b2286e0c5",
+    "url": "/ABZ2.github.io/img/banner-photo_1440.9f44e8d6.webp"
+  },
+  {
+    "revision": "0bdb9e09b1f87af6b21d996d269e97f3",
+    "url": "/ABZ2.github.io/img/banner-photo_1920.0bdb9e09.webp"
+  },
+  {
+    "revision": "ec993b11797b04d82dbe322dfa9fb331",
+    "url": "/ABZ2.github.io/img/banner-photo_2560.ec993b11.webp"
+  },
+  {
+    "revision": "a0b6a4367956ed5ab00acb08452017c5",
+    "url": "/ABZ2.github.io/img/banner-photo_3840.a0b6a436.webp"
+  },
+  {
+    "revision": "f91760978f0a271dda7de41a1cfca247",
+    "url": "/ABZ2.github.io/img/banner-photo_768.f9176097.webp"
+  },
+  {
+    "revision": "9780b0a3ddf42fbec07c5d22bb4a6028",
+    "url": "/ABZ2.github.io/img/banner-photo_992.9780b0a3.webp"
+  },
+  {
+    "revision": "32af61b74ae2dd2cd59e8c1004a41155",
+    "url": "/ABZ2.github.io/img/logo.32af61b7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/ABZ2.github.io/img/man-laptop-v1.f28cb247.svg"
+  },
+  {
+    "revision": "a33f2d3cee0ddafd303461d319e9a2ab",
+    "url": "/ABZ2.github.io/img/photo-cover.a33f2d3c.svg"
+  },
+  {
+    "revision": "d1375dc64779e61564785f507ab7fbf7",
+    "url": "/ABZ2.github.io/index.html"
+  },
+  {
+    "revision": "e4de488b2afd3fac326d",
+    "url": "/ABZ2.github.io/js/app.5903710f.js"
+  },
+  {
+    "revision": "86b13c24bc11597177e4",
+    "url": "/ABZ2.github.io/js/chunk-vendors.38a16b72.js"
+  },
+  {
+    "revision": "a4538c1ccfeaec9e79be49e6042cf296",
+    "url": "/ABZ2.github.io/manifest.json"
+  }
+]);
